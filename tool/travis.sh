c38b0fe172f3fc31ed3befda33c392aa86dfe9ab
German Saprykin <german.saprykin@it-nomads.com>
2019-04-19 01:55:41 +0800

    Fix travis.sh.

--- tool/travis.sh
+++ tool/travis.sh
@@ -13,9 +13,7 @@ pub get
 # Verify that the libraries are error and warning-free.
 echo "Running dartanalyzer..."
 dartanalyzer $DARTANALYZER_FLAGS \
-  bin/collect_coverage.dart \
-  bin/format_coverage.dart \
-  lib/coverage.dart
+  lib/
 
 # Verify that dartfmt has been run.
 echo "Checking dartfmt..."
@@ -34,20 +32,19 @@ if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "dev" ]; then
   echo "Collecting coverage on port $OBS_PORT..."
 
   # Start tests in one VM.
-  dart --disable-service-auth-codes \
-    --enable-vm-service=$OBS_PORT \
+  dart \
     --pause-isolates-on-exit \
-    test/test_all.dart &
+    test/* &
 
   # Run the coverage collector to generate the JSON coverage report.
-  dart bin/collect_coverage.dart \
+  pub run coverage \
     --port=$OBS_PORT \
     --out=var/coverage.json \
     --wait-paused \
     --resume-isolates
 
   echo "Generating LCOV report..."
-  dart bin/format_coverage.dart \
+  pub run coverage:format_coverage \
     --lcov \
     --in=var/coverage.json \
     --out=var/lcov.info \
