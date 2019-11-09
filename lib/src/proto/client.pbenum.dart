587433f98a64809bd3bc630d9b6d7fa558fc9f3b
synw <ansterj@gmail.com>
2019-11-09 15:36:51 +0100

    Linting: remove unecessary const keyworkds

--- lib/src/proto/client.pbenum.dart
+++ lib/src/proto/client.pbenum.dart
@@ -9,21 +9,20 @@ import 'dart:core' show int, dynamic, String, List, Map;
 import 'package:protobuf/protobuf.dart' as $pb;
 
 class MethodType extends $pb.ProtobufEnum {
-  static const MethodType CONNECT = const MethodType._(0, 'CONNECT');
-  static const MethodType SUBSCRIBE = const MethodType._(1, 'SUBSCRIBE');
-  static const MethodType UNSUBSCRIBE = const MethodType._(2, 'UNSUBSCRIBE');
-  static const MethodType PUBLISH = const MethodType._(3, 'PUBLISH');
-  static const MethodType PRESENCE = const MethodType._(4, 'PRESENCE');
-  static const MethodType PRESENCE_STATS =
-      const MethodType._(5, 'PRESENCE_STATS');
-  static const MethodType HISTORY = const MethodType._(6, 'HISTORY');
-  static const MethodType PING = const MethodType._(7, 'PING');
-  static const MethodType SEND = const MethodType._(8, 'SEND');
-  static const MethodType RPC = const MethodType._(9, 'RPC');
-  static const MethodType REFRESH = const MethodType._(10, 'REFRESH');
-  static const MethodType SUB_REFRESH = const MethodType._(11, 'SUB_REFRESH');
+  static const MethodType CONNECT = MethodType._(0, 'CONNECT');
+  static const MethodType SUBSCRIBE = MethodType._(1, 'SUBSCRIBE');
+  static const MethodType UNSUBSCRIBE = MethodType._(2, 'UNSUBSCRIBE');
+  static const MethodType PUBLISH = MethodType._(3, 'PUBLISH');
+  static const MethodType PRESENCE = MethodType._(4, 'PRESENCE');
+  static const MethodType PRESENCE_STATS = MethodType._(5, 'PRESENCE_STATS');
+  static const MethodType HISTORY = MethodType._(6, 'HISTORY');
+  static const MethodType PING = MethodType._(7, 'PING');
+  static const MethodType SEND = MethodType._(8, 'SEND');
+  static const MethodType RPC = MethodType._(9, 'RPC');
+  static const MethodType REFRESH = MethodType._(10, 'REFRESH');
+  static const MethodType SUB_REFRESH = MethodType._(11, 'SUB_REFRESH');
 
-  static const List<MethodType> values = const <MethodType>[
+  static const List<MethodType> values = <MethodType>[
     CONNECT,
     SUBSCRIBE,
     UNSUBSCRIBE,
@@ -49,13 +48,13 @@ class MethodType extends $pb.ProtobufEnum {
 }
 
 class PushType extends $pb.ProtobufEnum {
-  static const PushType PUBLICATION = const PushType._(0, 'PUBLICATION');
-  static const PushType JOIN = const PushType._(1, 'JOIN');
-  static const PushType LEAVE = const PushType._(2, 'LEAVE');
-  static const PushType UNSUB = const PushType._(3, 'UNSUB');
-  static const PushType MESSAGE = const PushType._(4, 'MESSAGE');
+  static const PushType PUBLICATION = PushType._(0, 'PUBLICATION');
+  static const PushType JOIN = PushType._(1, 'JOIN');
+  static const PushType LEAVE = PushType._(2, 'LEAVE');
+  static const PushType UNSUB = PushType._(3, 'UNSUB');
+  static const PushType MESSAGE = PushType._(4, 'MESSAGE');
 
-  static const List<PushType> values = const <PushType>[
+  static const List<PushType> values = <PushType>[
     PUBLICATION,
     JOIN,
     LEAVE,
