87ad2b1693782b4e5b56313abb211c1fc9038df7
synw <synw@users.noreply.github.com>
2020-04-14 13:56:49 +0200

    Add an example chat app (#32)
    Co-authored-by: synw <synw@yahoo.com>

---
+++ example/chat_app/lib/chat.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:dash_chat/dash_chat.dart';
+import 'package:flutter/material.dart';
+
+import 'conf.dart' as conf;
+import 'state.dart' as state;
+
+class _ChatPageState extends State<ChatPage> {
+  final _msgs = <ChatMessage>[];
+  StreamSubscription<ChatMessage> _sub;
+
+  @override
+  void initState() {
+    conf.cli.subscribe(conf.channel);
+    _sub = conf.cli.messages.listen((msg) => setState(() => _msgs.add(msg)));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DashChat(
+        messages: _msgs,
+        user: ChatUser(name: state.username),
+        onSend: (msg) async {
+          await conf.cli.sendMsg(msg);
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _sub.cancel();
+    super.dispose();
+  }
+}
+
+class ChatPage extends StatefulWidget {
+  const ChatPage();
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
